9a907f9cb5a24fab6436d1b82759031230982da8
Grafiters <bayugrafit@gmail.com>
2024-08-05 14:13:20 +0700

    first init

---
+++ config.js
@@ -0,0 +1,4 @@
+
+  export const nftmarketaddress = "0x123e46E2a1BEf22679b2591002Fd8EC23663bCB7"
+  export const nftaddress = "0x05A5b1bB2D4E5aEC0b7Fc7A09185991E76dfD7e9"
+  
---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+/* hardhat.config.js */
+require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
+// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 97
+    },
+  mumbai: {
+    url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
+    accounts: ["371d8f22b9a617c04eef587602c3ddb268e1fbfebe7601e79665d7e5011cf117"]
+  }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
